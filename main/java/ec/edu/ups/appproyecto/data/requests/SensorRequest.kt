91abf4cedbee8840e85e6490846f72b192faba4f
Vnmg1999 <109489696+Vnmg1999@users.noreply.github.com>
2022-07-17 21:12:31 -0500

    Delete main directory

--- main/java/ec/edu/ups/appproyecto/data/requests/SensorRequest.kt
+++
@@ -1,8 +0,0 @@
-package com.example.appregistro.data.requests
-
-import com.google.gson.annotations.SerializedName
-
-data class SensorRequest(
-    @SerializedName("Pulso")
-    var Pulso: String,
-    )
--- main/java/ec/edu/ups/appproyecto/data/responses/DefaultResponse.kt
+++
@@ -1,8 +0,0 @@
-package ec.edu.ups.appproyecto.data.responses
-
-
-data class DefaultResponse(
-    val code: Int,
-    val error: Boolean,
-    val message: String
-)
